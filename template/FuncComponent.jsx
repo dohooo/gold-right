9fefa6ef29d902dddf78c8e1cfbf03ad13d96a18
vkm0303 <1947055799@qq.com>
2022-06-01 22:33:52 +0800

    perf: add jsx template

---
+++ template/FuncComponent.jsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const [componentName] = props => {
+    return null
+}
---
+++ template/FuncComponent.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export interface[componentName]Props {
+
+}
+
+export const [componentName]: FC<[componentName]Props> = props => {
+    return null
+}
