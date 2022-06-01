42e99ef0ecc5c07a0744c82c7316cd15945e5402
vkm0303 <1947055799@qq.com>
2022-06-01 22:34:26 +0800

    perf: add class component's template

---
+++ template/ClassComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class [componentName] extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return null;
+  }
+}
---
+++ template/ClassComponent.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export interface [componentName]Props {
+  
+}
+
+export class [componentName] extends Component<[componentName]Props> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return null;
+  }
+}
